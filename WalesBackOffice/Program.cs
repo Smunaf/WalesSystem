eaf328abcded959be13bf6f08ebeb4faf7bea690
P2416625 <P2416625@GWHXX473WIN005W>
2020-03-10 15:54:27 +0000

    ToursForm

--- WalesBackOffice/Program.cs
+++ WalesBackOffice/Program.cs
@@ -16,7 +16,7 @@ namespace WalesBackOffice
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Tours());
         }
     }
 }
--- WalesBackOffice/Tours.cs
+++ WalesBackOffice/Tours.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WalesBackOffice
 {
-    public partial class Form1 : Form
+    public partial class Tours : Form
     {
-        public Form1()
+        public Tours()
         {
             InitializeComponent();
         }
